2766822587482ed291006741effd20c4156bdf26
Woorim-lee <woorim0806@gmail.com>
2021-11-06 19:09:55 +0900

    customer_retention_rate.sql

---
+++ customer_retention_rate.sql
@@ -0,0 +1,12 @@
+# 연도별 재구매율
+# 재구매율이란 특정기간 구매자 중 특정기간에 연달아 구매한 구매자의 비중
+
+select o.customerNumber,
+o.orderdate,
+o2.customerNumber,
+o2.orderdate
+from orders o
+left
+join  orders o2
+on o.customerNumber = o2.customerNumber 
+and substr(o.orderdate,1,4) = substr(o2.orderdate,1,4) -1;
