da54d822a25946b791e3f675bd7c5951ab2af0cf
Woorim-lee <woorim0806@gmail.com>
2021-11-08 12:06:20 +0900

    checking pull error

--- customer_retention_rate.sql
+++ customer_retention_rate.sql
@@ -10,3 +10,32 @@ left
 join  orders o2
 on o.customerNumber = o2.customerNumber 
 and substr(o.orderdate,1,4) = substr(o2.orderdate,1,4) -1;
+
+# 국가별 2004, 2005 retention rate(%) 구하기
+select c.country,
+substr(o.orderdate,1,4) yy,
+count(distinct o.customerNumber) buy_1,
+count(distinct o2.customerNumber) buy_2,
+count(distinct o2.customerNumber) / count(distinct o.customerNumber) retention_rate
+from orders o
+left
+join orders o2
+on o.customerNumber = o2.customerNumber
+and substr(o.orderdate,1,4) = substr(o2.orderdate,1,4) -1
+left
+join customers c
+on o.customerNumber = c.customerNumber
+group
+by 1,2;
+
+# 미국의 판매 top5 차량 모델 추출
+create table classicmodels.USA_CAR as
+select o.ordernumber, c.country, od.productcode, od.quantityOrdered
+from orders o
+left
+join customers c
+on o.customerNumber = c.customerNumber
+left
+join orderdetails od
+on o.orderNumber = od.orderNumber
+where c.country = 'USA';
